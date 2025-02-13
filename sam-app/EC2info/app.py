1cdc1d55c5bf0e6c0709b7c073967ab4bc20a5cd
dhineshkumar-d-trimble <dhineshkumar_d@trimble.com>
2025-02-13 12:47:00 +0530

    SAM Deployment

--- sam-app/EC2info/app.py
+++ sam-app/EC2info/app.py
@@ -11,27 +11,18 @@ def lambda_handler(event, context):
 
     for region in regions:
         ec2 = boto3.client('ec2', region_name=region)
-        response = ec2.describe_instances()
 
-        for reservation in response['Reservations']:
-            for instance in reservation['Instances']:
-                instance_info = {
-                    'InstanceId': instance.get('InstanceId'),
-                    'InstanceName': next((tag['Value'] for tag in instance.get('Tags', []) if tag['Key'] == 'Name'), 'N/A'),
-                    'AmiId': instance.get('ImageId'),
-                    'Region': region,
-                    'AvailabilityZone': instance.get('Placement', {}).get('AvailabilityZone'),
-                    'VpcId': instance.get('VpcId'),
-                    'SubnetId': instance.get('SubnetId'),
-                    'IpAddressRange': instance.get('PrivateIpAddress'),
-                    'State': instance.get('State', {}).get('Name')
-                }
-                all_instances.append(instance_info)
+        # Get all instances
+        response_all = ec2.describe_instances()
+        all_instances.extend(parse_instances(response_all, region))
 
-                if instance_info['State'] == 'running':
-                    running_instances.append(instance_info)
-                elif instance_info['State'] == 'stopped':
-                    stopped_instances.append(instance_info)
+        # Get running instances
+        response_running = ec2.describe_instances(Filters=[{'Name': 'instance-state-name', 'Values': ['running']}])
+        running_instances.extend(parse_instances(response_running, region))
+
+        # Get stopped instances
+        response_stopped = ec2.describe_instances(Filters=[{'Name': 'instance-state-name', 'Values': ['stopped']}])
+        stopped_instances.extend(parse_instances(response_stopped, region))
 
     return {
         'statusCode': 200,
@@ -41,3 +32,21 @@ def lambda_handler(event, context):
             'StoppedInstances': stopped_instances
         })
     }
+
+def parse_instances(response, region):
+    instances_info = []
+    for reservation in response['Reservations']:
+        for instance in reservation['Instances']:
+            instance_info = {
+                'InstanceId': instance.get('InstanceId'),
+                'InstanceName': next((tag['Value'] for tag in instance.get('Tags', []) if tag['Key'] == 'Name'), 'N/A'),
+                'AmiId': instance.get('ImageId'),
+                'Region': region,
+                'AvailabilityZone': instance.get('Placement', {}).get('AvailabilityZone'),
+                'VpcId': instance.get('VpcId'),
+                'SubnetId': instance.get('SubnetId'),
+                'IpAddressRange': instance.get('PrivateIpAddress'),
+                'State': instance.get('State', {}).get('Name')
+            }
+            instances_info.append(instance_info)
+    return instances_info
