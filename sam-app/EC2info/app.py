c68ca6f4fd1aa487ec245b368853c7b78332d770
dhineshkumar-d-trimble <dhineshkumar_d@trimble.com>
2025-02-13 13:13:48 +0530

    SAM Deployment

--- sam-app/EC2info/app.py
+++ sam-app/EC2info/app.py
@@ -35,74 +35,52 @@ def lambda_handler(event, context):
 
 def list_instances():
     try:
-        instances_info = get_instances_info()
-        return build_response(200, instances_info['AllInstances'])
+        response = ec2.describe_instances()
+        instances = extract_instance_details(response)
+        return build_response(200, instances)
     except Exception as e:
         print('Error:', e)
         return build_response(400, str(e))
 
 def list_running_instances():
     try:
-        instances_info = get_instances_info()
-        return build_response(200, instances_info['RunningInstances'])
+        response = ec2.describe_instances(Filters=[{'Name': 'instance-state-name', 'Values': ['running']}])
+        instances = extract_instance_details(response)
+        return build_response(200, instances)
     except Exception as e:
         print('Error:', e)
         return build_response(400, str(e))
 
 def list_stopped_instances():
     try:
-        instances_info = get_instances_info()
-        return build_response(200, instances_info['StoppedInstances'])
+        response = ec2.describe_instances(Filters=[{'Name': 'instance-state-name', 'Values': ['stopped']}])
+        instances = extract_instance_details(response)
+        return build_response(200, instances)
     except Exception as e:
         print('Error:', e)
         return build_response(400, str(e))
 
-def get_instances_info():
-    ec2_client = boto3.client('ec2')
-    regions = [region['RegionName'] for region in ec2_client.describe_regions()['Regions']]
-
-    all_instances = []
-    running_instances = []
-    stopped_instances = []
-
-    for region in regions:
-        ec2 = boto3.client('ec2', region_name=region)
-
-        # Get all instances
-        response_all = ec2.describe_instances()
-        all_instances.extend(parse_instances(response_all, region))
-
-        # Get running instances
-        response_running = ec2.describe_instances(Filters=[{'Name': 'instance-state-name', 'Values': ['running']}])
-        running_instances.extend(parse_instances(response_running, region))
-
-        # Get stopped instances
-        response_stopped = ec2.describe_instances(Filters=[{'Name': 'instance-state-name', 'Values': ['stopped']}])
-        stopped_instances.extend(parse_instances(response_stopped, region))
-
-    return {
-        'AllInstances': all_instances,
-        'RunningInstances': running_instances,
-        'StoppedInstances': stopped_instances
-    }
-
-def parse_instances(response, region):
-    instances_info = []
+def extract_instance_details(response):
+    instances = []
     for reservation in response['Reservations']:
         for instance in reservation['Instances']:
-            instance_info = {
+            instance_details = {
                 'InstanceId': instance.get('InstanceId'),
-                'InstanceName': next((tag['Value'] for tag in instance.get('Tags', []) if tag['Key'] == 'Name'), 'N/A'),
+                'InstanceName': get_instance_name(instance),
                 'AmiId': instance.get('ImageId'),
-                'Region': region,
+                'Region': instance.get('Placement', {}).get('AvailabilityZone')[:-1],
                 'AvailabilityZone': instance.get('Placement', {}).get('AvailabilityZone'),
                 'VpcId': instance.get('VpcId'),
-                'SubnetId': instance.get('SubnetId'),
-                'IpAddressRange': instance.get('PrivateIpAddress'),
-                'State': instance.get('State', {}).get('Name')
+                'SubnetId': instance.get('SubnetId')
             }
-            instances_info.append(instance_info)
-    return instances_info
+            instances.append(instance_details)
+    return instances
+
+def get_instance_name(instance):
+    for tag in instance.get('Tags', []):
+        if tag['Key'] == 'Name':
+            return tag['Value']
+    return None
 
 class CustomEncoder(json.JSONEncoder):
     def default(self, obj):
