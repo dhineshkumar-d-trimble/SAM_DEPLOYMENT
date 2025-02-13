1ceec21e5192d4b0f2f39e9c7f7b75816e777d46
dhineshkumar-d-trimble <dhineshkumar_d@trimble.com>
2025-02-13 12:52:09 +0530

    SAM Deployment

--- sam-app/EC2info/app.py
+++ sam-app/EC2info/app.py
@@ -1,7 +1,63 @@
 import json
 import boto3
+from datetime import datetime
+
+# Initialize the EC2 client
+ec2 = boto3.client('ec2')
+
+# Define the paths for the endpoints
+list_instances_path = '/list-instances/info'
+running_instances_path = '/running-instances/info'
+stopped_instances_path = '/stopped-instances/info'
 
 def lambda_handler(event, context):
+    print('Request event: ', event)
+    response = None
+
+    try:
+        http_method = event.get('httpMethod')
+        path = event.get('path')
+
+        if http_method == 'GET' and path == list_instances_path:
+            response = list_instances()
+        elif http_method == 'GET' and path == running_instances_path:
+            response = list_running_instances()
+        elif http_method == 'GET' and path == stopped_instances_path:
+            response = list_stopped_instances()
+        else:
+            response = build_response(404, '404 Not Found')
+
+    except Exception as e:
+        print('Error:', e)
+        response = build_response(400, 'Error processing request')
+
+    return response
+
+def list_instances():
+    try:
+        instances_info = get_instances_info()
+        return build_response(200, instances_info['AllInstances'])
+    except Exception as e:
+        print('Error:', e)
+        return build_response(400, str(e))
+
+def list_running_instances():
+    try:
+        instances_info = get_instances_info()
+        return build_response(200, instances_info['RunningInstances'])
+    except Exception as e:
+        print('Error:', e)
+        return build_response(400, str(e))
+
+def list_stopped_instances():
+    try:
+        instances_info = get_instances_info()
+        return build_response(200, instances_info['StoppedInstances'])
+    except Exception as e:
+        print('Error:', e)
+        return build_response(400, str(e))
+
+def get_instances_info():
     ec2_client = boto3.client('ec2')
     regions = [region['RegionName'] for region in ec2_client.describe_regions()['Regions']]
 
@@ -25,12 +81,9 @@ def lambda_handler(event, context):
         stopped_instances.extend(parse_instances(response_stopped, region))
 
     return {
-        'statusCode': 200,
-        'body': json.dumps({
-            'AllInstances': all_instances,
-            'RunningInstances': running_instances,
-            'StoppedInstances': stopped_instances
-        })
+        'AllInstances': all_instances,
+        'RunningInstances': running_instances,
+        'StoppedInstances': stopped_instances
     }
 
 def parse_instances(response, region):
@@ -50,3 +103,18 @@ def parse_instances(response, region):
             }
             instances_info.append(instance_info)
     return instances_info
+
+class CustomEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, datetime):
+            return obj.isoformat()
+        return super(CustomEncoder, self).default(obj)
+
+def build_response(status_code, body):
+    return {
+        'statusCode': status_code,
+        'headers': {
+            'Content-Type': 'application/json'
+        },
+        'body': json.dumps(body, cls=CustomEncoder)
+    }
